2a8928684bb5dd5f99d3aee4b681a9c5ae457f9b
EmilyCook321 <emilyacook16@gmail.com>
2022-04-18 12:40:19 +0100

    Watchers

--- App.vue
+++ App.vue
@@ -1,12 +1,35 @@
 <script>
-let id = 0
+let id = 0;
 export default {
-mounted (){
-  this.$refs.p.textContent = 'mounted!'
-}
-}
+  data() {
+    return {
+      todoId: 1,
+      todoData: null,
+    };
+  },
+  methods: {
+    async fetchData() {
+      this.todoData = null;
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${this.todoId}`
+      );
+      this.todoData = await res.json();
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+  watch: {
+    todoId() {
+      this.fetchData();
+    },
+  },
+};
 </script>
 
 <template>
-<p ref="p">hello</p>
+  <p>Todo id: {{ todoId }}</p>
+  <button @click="todoId++">Fetch next tofo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }} </pre>
 </template>
